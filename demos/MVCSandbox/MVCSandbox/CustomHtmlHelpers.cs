20b72cc6f7dd287b94f30b54f4b5cefcf43b2d22
Denis Kyashif <denis.kyashif@gmail.com>
2018-03-05 00:46:38 +0200

    created demos/MVCSandbox app

---
+++ demos/MVCSandbox/MVCSandbox/CustomHtmlHelpers.cs
@@ -0,0 +1,34 @@
+﻿namespace MVCSandbox
+{
+    using System.Collections.Generic;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlString List(this HtmlHelper helper, IEnumerable<string> items, bool ordered = false)
+        {
+            var list = new TagBuilder(ordered ? "ol" : "ul");
+            
+            foreach (var item in items)
+            {
+                var li = new TagBuilder("li");
+                li.SetInnerText(item);
+                list.InnerHtml += li.ToString();
+            }
+            
+            return new MvcHtmlString(list.ToString());
+        }
+
+        public static IHtmlString Button(this HtmlHelper helper, string text, object attributes = null)
+        {
+            var button = new TagBuilder("button");
+            button.SetInnerText(text);
+
+            // converts an anonymous object to a dictionary
+            button.MergeAttributes(new RouteValueDictionary(attributes));
+            return new MvcHtmlString(button.ToString());
+        }
+    }
+}
