1f4f04043675143726eb1aff3aad09be1014586c
Denis Kyashif <denis.kyashif@gmail.com>
2018-03-05 19:46:05 +0200

    updated demos/MovieStore

---
+++ demos/MovieStore/MovieStore/TagHelpers/HelloTagHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieStore.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System;
+
+    [HtmlTargetElement("hi")]
+    public class HelloTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            output.Attributes.Add("id", context.UniqueId);
+
+            output.PreContent.SetContent("Hello ");
+            output.PostContent.SetContent($", time is now: {DateTime.Now.ToString("HH: mm")}");
+        }
+    }
+}
---
+++ demos/MovieStore/MovieStore/TagHelpers/ListTagHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieStore.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Collections.Generic;
+    
+    [HtmlTargetElement("my-list")]
+    public class ListTagHelper : TagHelper
+    {
+        [HtmlAttributeName("items")]
+        public IEnumerable<object> Items { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            foreach (var item in this.Items)
+            {
+                output.Content.AppendHtml($"<li>{item}</li>");
+            }
+        }
+    }
+}
