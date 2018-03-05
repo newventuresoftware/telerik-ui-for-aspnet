c26ac2d8fb9d55dd4e321cb0213c34820f250dde
Denis Kyashif <denis.kyashif@gmail.com>
2018-03-05 23:19:10 +0200

    modified slides and added dummy data to MovieStore

--- demos/MovieStore/MovieStore/DataContext.cs
+++ demos/MovieStore/MovieStore/DataContext.cs
@@ -15,6 +15,41 @@
                 Rating = 8,
                 Genre = "Thriller"
             },
+            new Movie()
+            {
+                Title = "Scent of a Woman",
+                ReleaseDate = new DateTime(1993, 1, 1),
+                Rating = 7,
+                Genre = "Drama"
+            },
+            new Movie()
+            {
+                Title = "The Departed",
+                ReleaseDate = new DateTime(2006, 1, 1),
+                Rating = 8,
+                Genre = "Crime"
+            },
+            new Movie()
+            {
+                Title = "2001: A Space Odyssey",
+                ReleaseDate = new DateTime(1968, 1, 1),
+                Rating = 8,
+                Genre = "Sci-Fi"
+            },
+            new Movie()
+            {
+                Title = "It",
+                ReleaseDate = new DateTime(2017, 1, 1),
+                Rating = 7,
+                Genre = "Horror"
+            },
+            new Movie()
+            {
+                Title = "Vanilla Sky",
+                ReleaseDate = new DateTime(2001, 1, 1),
+                Rating = 6,
+                Genre = "Drama"
+            }
         };
     }
 }
