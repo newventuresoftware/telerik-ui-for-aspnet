775bfe490e04981c29424a7db825e32c2ffc219c
Denis Kyashif <denis.kyashif@gmail.com>
2018-03-05 22:48:18 +0200

    MovieStore - added a grid do display the movies

--- demos/MovieStore/MovieStore/Pages/AddMovie.cshtml.cs
+++ demos/MovieStore/MovieStore/Pages/AddMovie.cshtml.cs
@@ -3,12 +3,17 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
     using MovieStore.Models;
+    using System.Collections.Generic;
 
     public class AddMovie : PageModel
     {
         [BindProperty]
         public Movie Movie { get; set; }
-        
+
+        [BindProperty]
+        public IEnumerable<string> Genres { get; set; } = 
+            new string[] { "Action", "Drama", "Comedy", "Horror", "Documentary", "Thriller", "Romance" };
+
         public IActionResult OnPost(Movie movie)
         {
             if (!ModelState.IsValid)
@@ -18,12 +23,6 @@
 
             DataContext.Movies.Add(movie);
             return RedirectToPage("Movies");
-        }
-
-
-        public IActionResult GetGenres()
-        {
-            return new JsonResult(new string[] { "One", "Two" });
         }
     }
 }
