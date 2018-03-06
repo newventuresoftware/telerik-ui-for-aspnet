d76f0e216c6a763e68a4ac6ece4a1962efe9abda
Denis Kyashif <denis.kyashif@gmail.com>
2018-03-06 17:02:44 +0200

    added notes to the slides and the demos

--- demos/MovieStore/MovieStore/Pages/AddMovie.cshtml.cs
+++ demos/MovieStore/MovieStore/Pages/AddMovie.cshtml.cs
@@ -7,11 +7,16 @@
 
     public class AddMovie : PageModel
     {
+        /* Razor Pages introduce a new model-binding attribute, [BindProperty], 
+         * which is especially useful on forms.You can apply this attribute to a property 
+         * on a Razor Page (with or without an explicit PageModel) to opt into data binding 
+         * for non-GET requests to the page.This enables tag helpers like asp-for and asp-validation-for
+         * to work with the property you’ve specified, and allows handlers to work with bound properties 
+         * without having to specify them as method parameters. The [BindProperty] attribute also works on Controllers.*/
         [BindProperty]
         public Movie Movie { get; set; }
 
-        [BindProperty]
-        public IEnumerable<string> Genres { get; set; } = 
+        public IEnumerable<string> Genres { get; set; } =
             new string[] { "Action", "Drama", "Comedy", "Horror", "Documentary", "Thriller", "Romance" };
 
         public IActionResult OnPost(Movie movie)
